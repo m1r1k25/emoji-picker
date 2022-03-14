3fff08cbfc40e6f6e0729bddebbd52aaf6e65520
Miroslav Zaitsev <m1r1k25@mail.ru>
2022-03-14 11:32:09 +0300

    init: init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import EmojiContainer from './Components/EmojiContainer/EmojiContainer'
+import Input from './Components/Input/Input'
+
+function App() {
+  return (
+    <div>
+      <EmojiContainer />
+      <Input />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/EmojiContainer/EmojiContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmojiContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default EmojiContainer;
